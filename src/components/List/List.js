5f2d10265ccb9486f13c1f4e12afa825fca1d204
chang0022 <chang.0022@163.com>
2017-08-29 16:34:36 +0800

    还是不太明白

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,13 +4,22 @@ import './List.css';
 import ListItem from '../ListItem/ListItem';
 
 class List extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            curr: ''
+        }
+    }
+
     render() {
         const {lists} = this.props;
         return (
             <div className="page__bd">
                 <ul>
                     {lists.map((item,index) => (
-                        <ListItem item={item} key={index} />
+                        <ListItem item={item} index={index} key={index} />
                     ))}
                 </ul>
             </div>
--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
 class ListItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            curr: ''
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return !(nextState.curr === this.state.curr)
+    }
+
+    handleClick(i) {
+        this.setState({curr: i})
+    }
+
     render() {
-        const {item} = this.props;
+        const {item, index} = this.props;
         return (
-            <li>
+            <li
+                className={index === this.state.curr ? 'js-show' : ''}
+                onClick={this.handleClick.bind(this, index)}>
                 <div className="weui-flex">
                     <p className="weui-flex__item">{item.name}</p>
-                    <img src={item.icon} alt={item.name} />
+                    <img src={item.icon} alt={item.name}/>
                 </div>
             </li>
         );
